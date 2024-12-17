9347587d6c2ba7e5205a6a1517361b1afa037356
Satya Prasad Metta <satya.prasadmetta@ihg.com>
2024-12-17 08:53:51 +0530

    Initial Spring Code for project

---
+++ src/main/java/com/Satya/Employee/Register/EmployeeRegisterApplication.java
@@ -0,0 +1,13 @@
+package com.Satya.Employee.Register;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeRegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeRegisterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Satya/Employee/Register/EmployeeRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Satya.Employee.Register;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
